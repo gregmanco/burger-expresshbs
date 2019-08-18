89496b4895a11d1de8f63baa79c56847d1735777
Greg Manco <gregmanco@gmail.com>
2019-08-18 17:06:49 -0400

    Created connection, schema and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ('Smokehouse');
+INSERT INTO burgers (name) VALUES ('Tex-Mex');
+INSERT INTO burgers (name, devoured) VALUES ('Baconator', true);
+INSERT INTO burgers (name, devoured) VALUES ('Big-Mac', true);
